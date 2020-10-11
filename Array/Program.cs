27389fde9339a67dd54eb72e93eddd46a306beec
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-11 23:29:26 +0300

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "stop" && input != "")
+            {
+                string inputType = "";
+                bool onlyDigits = true;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] < '0' || input[i] > '9')
+                        onlyDigits = false;
+                }
+
+                if (onlyDigits)
+                {
+                    inputType = "numar";
+                }
+
+                if (input == "true" || input == "false")
+                {
+                    inputType = "valoare booleana";
+                }
+
+                if (inputType == "")
+                {
+                    inputType = "text";
+                }
+
+                Console.WriteLine(inputType);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
