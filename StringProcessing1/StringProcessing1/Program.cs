05c850cce4119bfbe384f0ed9b8728fefe0d31e0
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-13 21:35:22 +0300

    solving string comparation using Contains()

---
+++ StringProcessing1/StringProcessing1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+/*
+Se dă un text pe o linie și o literă pe următoarea linie. Să se scrie o aplicație consolă ce determină dacă litera dată se află în text sau nu.
+Exemplu:
+Pentru datele de intrare:
+sample text
+a
+La consolă se va afișa:
+True*/
+
+using System;
+
+namespace StringProcessing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+
+            string ch = Console.ReadLine();
+
+            Console.WriteLine(inputData.Contains(ch));
+        }
+
+    }
+            
+ }
+        
+    
+
