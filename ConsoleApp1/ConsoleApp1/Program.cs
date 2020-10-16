8deff501cf75b9cfd07cb6663b17c67fe5684c10
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-16 22:06:56 +0300

    app that finds the "=" and replace it with a new line,  used Replace()

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Se dă pe o linie o intrare dintr-un dicționar englez-român astfel:
+<cuvânt în limba engelză>=<traducerea în limba română>
+Să se scrie o aplicație consolă ce extrage și tipărește pe câte o linie cuvântul în limba engleză și traducerea în limba română.
+Exemplu:
+Pentru datele de intrare:
+blue=albastru
+La consolă se va afișa:
+blue
+albastru
+ */
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            string result = inputString.Replace("=", "\n");
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
