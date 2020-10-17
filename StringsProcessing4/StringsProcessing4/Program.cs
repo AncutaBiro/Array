a2e07e8001c5052aea4abf7d1890dd24e099fd85
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-17 23:04:59 +0300

    using Split() to create a String[] then return the requested word no

---
+++ StringsProcessing4/StringsProcessing4/Program.cs
@@ -0,0 +1,45 @@
+﻿
+/*
+ Se dă pe o linie o frază compusă din mai multe cuvinte despărțite între ele printr-un singur spațiu. Pe următoarea linie se dă un număr X.
+Să se scrie o aplicație consolă ce tipărește al X-lea cuvânt din frază, dacă fraza are cel puțin X cuvinte. Dacă fraza nu are X cuvinte, atunci tipărește N/A.
+
+Exemplu:
+Pentru datele de intrare:
+o fraza de test
+3
+La consolă se va afișa:
+de
+ */
+
+using System;
+
+namespace StringsProcessing4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputString = Console.ReadLine();
+
+            string inputString2 = Console.ReadLine();
+            int wordsPosition = Convert.ToInt32(inputString2);
+
+            string[] result = inputString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (wordsPosition < result.Length + 1 && wordsPosition > 0)
+            {
+                Console.WriteLine(result[wordsPosition - 1]);
+            }
+            else
+            {
+                Console.WriteLine("N/A");
+            }
+
+            }
+
+        }
+
+    }
+
+
