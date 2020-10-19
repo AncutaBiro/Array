2068a528cdacf259f7d303463dcc64c931a488ca
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-19 23:05:34 +0300

    concatenate 2 strings using for loop then ToString() to result a new string

---
+++ StringsDebugging1/StringsDebugging1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringsDebugging1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encodedMessageFirstPart = Console.ReadLine();
+            string encodedMessageSecondPart = Console.ReadLine();
+            string message = "";
+
+            for (int i = 0; i < encodedMessageSecondPart.Length; i++)
+            {
+                message += encodedMessageFirstPart[i].ToString() + encodedMessageSecondPart[i].ToString();
+            }
+
+            if (encodedMessageFirstPart.Length > encodedMessageSecondPart.Length)
+            {
+                message += encodedMessageFirstPart[encodedMessageFirstPart.Length - 1].ToString();
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
