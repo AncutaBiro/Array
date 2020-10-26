18622784e7ac9f76648ea15b53c7160c3a023666
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-26 23:30:45 +0200

    still working to solve the N/A clause

---
+++ Arrays3/Arrays3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays3
+{
+
+/*
+ inca lucrez
+ */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] greenTeam = new string[3];
+            string[] redTeam = new string[3];
+
+            for (int i = 0; i < greenTeam.Length; i++)
+            {
+                greenTeam[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < redTeam.Length; i++)
+            {
+                redTeam[i] = Console.ReadLine();
+            }
+
+            string bothTeams = redTeam.Concat(greenTeam).ToArray().ToString();
+
+           
+
+            string requestName = Console.ReadLine();
+
+            Console.WriteLine(requestName);
+
+            Console.WriteLine(bothTeams);
+
+            Console.WriteLine(bothTeams.Contains(requestName));
+
+            if (bothTeams.Contains(requestName))
+            {
+
+                for (int i = 0; i < greenTeam.Length; i++)
+                {
+
+                    if (requestName == greenTeam[i])
+                    {
+                        Console.WriteLine("echipa verde");
+                    }
+                }
+
+                for (int i = 0; i < redTeam.Length; i++)
+                {
+
+                    if (requestName == redTeam[i])
+                    {
+                        Console.WriteLine("echipa rosie");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("N/A");
+            }
+
+        }
+    }
+}
