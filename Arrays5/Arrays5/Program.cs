f69bb270ba624b51e9ec0ec88f4ae129ae9dd081
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-26 23:42:09 +0200

    solved

---
+++ Arrays5/Arrays5/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Mai sus avem un triunghi format din 4 nivele. Determină numărul total de puncte de intersecție într-un triunghi cu n nivele.
+Exemplu:
+Pentru datele de intrare:
+4
+La consolă se va afișa:
+15
+ */
+using System;
+
+namespace Arrays5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputdata = Console.ReadLine();
+            int n = Convert.ToInt32(inputdata);
+
+            int sum = 0;
+
+            for (int i = 0; i <= n; i++) 
+            {
+                sum += i + 1;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
