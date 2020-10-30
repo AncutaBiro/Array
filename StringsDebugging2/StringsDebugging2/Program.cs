3a341fc966e41a6cb3ee3ff6197e6c4cd4389ef2
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-30 23:15:44 +0200

    debugging solved

--- StringsDebugging2/StringsDebugging2/Program.cs
+++ StringsDebugging2/StringsDebugging2/Program.cs
@@ -14,6 +14,7 @@ La consolă se va afișa:
 
 using System;
 
+
 namespace StringsDebugging2
 {
     class Program
@@ -25,30 +26,62 @@ namespace StringsDebugging2
 
             int count = 0;
 
-            for (int i = 0; i < text.Length - 1; i++)
+            for (int i = 0; i <= text.Length - textToFind.Length; i++)
             {
                 bool match = true;
 
-                for (int j = 0; j < textToFind.Length ; j++)
+                for (int j = 0; j < textToFind.Length; j++)
                 {
-                    if (text[i+j] != textToFind[j])
+                    if (text[i + j] != textToFind[j])
                     {
                         match = false;
-                        continue;
+                        break;
                     }
-                 
+
                 }
 
                 if (match)
                 {
                     count++;
                 }
-        
 
             }
 
             Console.WriteLine(count);
             Console.Read();
+
+
+        /* https://www.geeksforgeeks.org/frequency-substring-string/ */ 
+
+            /*
+             int M = pat.Length;        
+                    int N = txt.Length;        
+                    int res = 0;
+
+                    /* A loop to slide pat[] one by one */
+            /*            for (int i = 0; i <= N - M; i++)*/
+            /*{*/
+            /* For current index i, check for 
+        pattern match */
+            /*int j;
+            for (j = 0; j < M; j++)
+            {
+                if (txt[i + j] != pat[j])
+                {
+                    break;
+                }
+            }
+*/
+            // if pat[0...M-1] = txt[i, i+1, ...i+M-1] 
+            /* if (j == M)
+             {
+                 res++;
+                 j = 0;
+             }
+         }*/
+            /*            return res;*/
+            /*       }*/
+
 
         }
     }
