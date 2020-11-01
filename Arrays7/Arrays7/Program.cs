168a630de447797a8a4e471aadd2954f805af875
AncutaBiro <ancuta.biro@yahoo.com>
2020-11-01 16:46:19 +0200

    solved debugging

---
+++ Arrays7/Arrays7/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Transactions;
+
+namespace Arrays7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int noStores = 2;
+
+            int trimesters = 4;
+
+            double[] doubleArray = new double [100];
+
+
+
+            int[,] matrix = new int [noStores, trimesters];/*
+*/
+            while (noStores > 0)
+            {
+                
+                string [] input = Console.ReadLine().Split();
+
+/*                if (input != '\n')*/
+                {
+                    doubleArray = Array.ConvertAll(input, double.Parse);
+                }
+
+                foreach (double i in doubleArray)
+                    Console.WriteLine(i);
+
+
+              /*  string[] stringArray = input.ToString("F", input.Split(" "));
+
+                for (int i = 0; i < stringArray.Length; i++)
+                {
+                    doubleArray[i] = Math.Round (Convert.ToDouble(stringArray[i]), 2);
+                    Console.Write(doubleArray[i]);
+                }
+*/
+                /* while (input != " " && char.Parse(input) != '\n')
+                 {
+                     for (int i = 0; i < input.Length; i++)
+                     {
+                         doubleArray [i] = Convert.ToDouble(input);
+                     }
+                 }
+
+                 noStores--;
+              }*/
+
+            }
+
+
+                
+            /*            }*/
+
+           /* foreach (int i in matrix)
+            {
+                foreach (int j in matrix) 
+                { 
+
+                Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine(" "); 
+            }*/
+            
+           
+        }
+    }
+}
---
+++ StringDebugging3/StringDebugging3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Să se scrie o aplicație consolă ce extrage numerele întregi dintr-o frază dată și le afișează pe toate pe o linie, despărțite printr-un spațiu.
+Sugestie: Folosiți execuția pas cu pas pentru a localiza și fixa eroarea din proiectul atașat.
+
+Exemplu:
+Pentru datele de intrare:
+4 ore pe zi, 5 zile pe saptamana
+La consolă se va afișa:
+4 5
+ */
+
+using System;
+
+namespace NumbersExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string numbers = "";
+            bool digitAdded = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] >= '0' && text[i] <= '9')
+                {
+                    numbers += text[i];
+
+ /*                   digitAdded = !digitAdded;*/
+/*                    because after checking the first numbers it becomes not True == false and never enters the else clause, so not adding space beetween the numbers*/
+
+                    digitAdded = true;
+                }
+                else
+                {
+                    if (digitAdded)
+                    {
+                        numbers += " ";
+                        digitAdded = false;
+                    }
+                }
+                
+            }
+            Console.WriteLine(numbers);
+            Console.ReadLine();
+        }
+    }
+}
+
